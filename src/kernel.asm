a6eab59a70aa502e76b38890400691458f4c6701
Julian Mendez <julian.mend34@gmail.com>
2013-12-07 22:31:01 +0100

    Add GDT init call and multiboot headers in asm kernel

--- src/kernel.asm
+++ src/kernel.asm
@@ -1,39 +1,63 @@
 [BITS 32]               ; indique a nasm que l'on travaille en 16 bits
 [ORG 0x1000]            ; Offset à ajouter aux adresses référencées
 
-jmp start
+%define MULTIBOOT_HEADER_MAGIC  0x1BADB002
+%define MULTIBOOT_HEADER_FLAGS  0x00000003
+%define CHECKSUM - (MULTIBOOT_HEADER_MAGIC +MULTIBOOT_HEADER_FLAGS)
+
+_start:
+    jmp start
+
+align 4
+multiboot_header:
+    dd MULTIBOOT_HEADER_MAGIC
+    dd MULTIBOOT_HEADER_FLAGS
+    dd CHECKSUM
 
 %include "inc/STDIO.INC"
+%include "inc/GDT.INC"
 
 start:
-	call _clear
+    
+    call _init_kernel_gdt
+    mov word ax, 0x18
+    mov word ss, ax
+    mov dword esp, 0x20000
+    call _main
 
-	mov eax, 5
-	push eax
-	push eax
-	call _gotoxy
-	pop eax
-	pop eax
+    cli     ; Clear interrupts
+    hlt     ; Halt the CPU
 
-	mov eax, msg
-	push eax
-	call _printk
-	pop eax
+_main:
+    call _clear
 
-	mov ax, 0x04
-	push ax
-	call _setattr
-	pop ax
+    push dword 0x00
+    push dword 0x00
+    call _gotoxy
+    add esp, 8
 
-	mov eax, msg
-	push eax
-	call _printk
-	pop eax
+    push dword msg00
+    call _printk
+    add esp, 4
+
+    push dword 0x04
+    call _setattr
+    add esp, 4
+
+    push dword 0x01
+    push dword 0x00
+    call _gotoxy
+    add esp, 8
+
+    push dword msg01
+    call _printk
+    add esp, 4
 
 ;Boucle infinie
 end:
 	jmp end
 
 ;----------------------------------
-msg: db "Message de test suffisament long pour depasser les 80 caracteres et tester si on arrive a retourner a la ligne. Ca serait super ! :)", 0
+msg00: db "Kernel is running ...", 0
+msg01: db "Hello world ! ", 0
 ;----------------------------------
